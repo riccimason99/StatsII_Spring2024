a7b643c17a0eabc3c507fb08e3f1cb31bf024706
riccimason99 <144854189+riccimason99@users.noreply.github.com>
2024-02-11 23:09:35 +0000

    My Answers PS1
    My Answers PS1

--- problemSets/PS01/My_Answers/PS1.R
+++ problemSets/PS01/My_Answers/PS1.R
@@ -38,11 +38,43 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #####################
 
 set.seed(123)
-# create empirical distribution of observed data
-ECDF <- ecdf(data)
-empiricalCDF <- ECDF(data)
+
+# Create both datasets
+cauchyData <- rcauchy(1000, location = 0, scale = 1)
+ECDF <- ecdf(cauchyData)
+empiricalCDF <- ECDF(cauchyData)
 # generate test statistic
-D <- max(abs(empiricalCDF - pnorm(data)))
+D <- max(abs(empiricalCDF - pnorm(cauchyData)))
+
+
+ks_tst <- function(data) {
+  ECDF <- ecdf(data)
+  empiricalCDF <- ECDF(data)
+  # generate test statistic
+  D <- max(abs(empiricalCDF - pnorm(data)))
+  #####
+  #
+  n <- length(data)
+  sum1 <- 0 
+  for (i in 1:n) {   # loop through each value
+  # get the summation
+  sum1 <- sum1 + exp(-(2*i-1)^2 * pi^2 / (8*D^2))
+  }
+  # solve the first part of the equation
+  pval <- (sqrt(2*pi)/D) * sum1 
+  # Return the t-stat and p value
+  answer <- list(D = D, P_Value = pval)
+  return(answer)
+}
+
+# Test my function 
+cauchyData <- rcauchy(1000, location = 0, scale = 1)
+ks_tst(cauchyData)
+
+# Test run the built in funciton to compare
+ks.test(cauchyData, "pnorm")
+
+
 
 #####################
 # Problem 2
@@ -51,3 +83,36 @@ D <- max(abs(empiricalCDF - pnorm(data)))
 set.seed (123)
 data <- data.frame(x = runif(200, 1, 10))
 data$y <- 0 + 2.75*data$x + rnorm(200, 0, 1.5)
+View(data)
+
+### Test with lm()###
+mod1 <- lm(y~x, data = data)
+summary(mod1)
+
+
+### Make link function###
+lin_link <- function(theta, y, x){
+  n <- nrow(x)
+  k <- ncol(x)
+  beta <- theta[1:k] # our predictors
+  sigma2 <- theta[k+1]^2 # calculate the variance of error 
+  e <- y-x%*%beta # compute the residuals
+  # log likelihood equation
+  logl <- -.5*n*log(2*pi)-.5*n*log(sigma2) - ((t(e) %*% 
+      e)/ (2*sigma2))
+  return(-logl)
+}
+
+
+linear_MLE <- optim(
+  fn = lin_link,
+  par = c(1, 1, 1), # initial parameters
+  hessian = TRUE,
+  y = data$y,
+  x = cbind(1, data$x),
+  method = "BFGS"
+)
+
+linear_MLE$par
+
+?optim
